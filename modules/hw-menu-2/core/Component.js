4f331a8e96b37aad96b9da7256c74a8a2dbaaab3
Vika Mironenko <mironenko-vika-2010@mail.ru>
2022-11-20 11:39:45 +0300

    completed hw

---
+++ modules/hw-menu-2/core/Component.js
@@ -0,0 +1,41 @@
+export class Component extends HTMLElement {
+    constructor() {
+        super();
+        this.props = {};
+        this.state = {};
+    }
+
+    setState(callback) {
+        this.state = callback(this.state);
+        this.innerHTML = this.render();
+    }
+
+    connectedCallback() {
+        this.innerHTML = this.render();
+        this.componentDidMount();
+
+    }
+
+    disconnectedCallback() {
+        this.componentWillUnmount();
+
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.componentWillUpdate(name, oldValue, newValue);
+        this.getAttributeNames().forEach((name) => {
+            this.props[name] = this.getAttribute(name);
+        });
+
+    }
+
+    dispatch(type, props) {
+        this.dispatchEvent(new CustomEvent(type, { bubbles: true, detail: props }));
+
+    }
+    
+    render() { };
+    componentDidMount() { };
+    componentWillUnmount() { };
+    componentWillUpdate() { };
+}
